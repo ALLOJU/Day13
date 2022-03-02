491f0558353eb19c7a173ae9b6132ab3126a27aa
ALLOJU <mounika.alloju@gmail.com>
2022-03-02 16:23:13 +0530

    UC3 added

--- src/MaximumTest.java
+++ src/MaximumTest.java
@@ -1,36 +1,20 @@
 
-public class MaximumTest {
+public class MaximumTest<T extends Comparable<T>> {
 
-	public static void main(String[] args) {
+	T x,y,z;
+	
+	public MaximumTest(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 
-		//declaring values
-		Integer x=5;
-		Integer y=6;
-		Integer z=8;
-		Float xf=6.6f,yf=8.8f,zf=7.7f;
-		MaximumTest.testMaximum(x,y,z);
-		MaximumTest.testMaximum(xf, yf, zf);
+	public T maximum() {
+		return MaximumTest.maximum(x,y,z);
 	}
-	private static float testMaximum(Float xf, Float yf, Float zf) {
-		
-		Float max=xf;
-		if(yf.compareTo(max)>0) {
-			max=yf;
-		}
-		if(zf.compareTo(max)>0) {
-			max=zf;
-		}
-		printMax(xf,yf,zf,max);
-		return max;
-		
-	}
-	private static void printMax(Float xf, Float yf, Float zf, Float max) {
-		System.out.printf("Max of %s ,%s and %s is %s \n",xf,yf,zf,max);
-		
-	}
-	//Method to find maximum of 3 numbers
-	public static int testMaximum(Integer x, Integer y, Integer z) {
-		Integer max=x;
+	public static <T extends Comparable<T>> T  maximum(T x,T y,T z ) {
+
+		T max=x;
 		if(y.compareTo(max)>0) {
 			max=y;
 		}
@@ -40,9 +24,42 @@ public class MaximumTest {
 		printMax(x,y,z,max);
 		return max;
 	}
-	//Method to print integer  values
-	private static void printMax(Integer x, Integer y, Integer z, Integer max) {
-		System.out.printf("Max of %s ,%s and %s is %s \n",x,y,z,max);
+	public static String   testMaximum(String x,String y,String z ) {
+
+		String max=x;
+		if(y.compareTo(max)>0) {
+			max=y;
+		}
+		if(z.compareTo(max)>0) {
+			max=z;
+		}
+		printMax(x,y,z,max);
+		return max;
+	}
+
+	public static <T> void printMax(T x, T y, T z, T max) {
+		System.out.printf("Max of %s , %s and %s is %s\n",x,y,z,max);
+	}
+	
+	public static void main(String[] args) {
+
+		//declaring values
+		Integer x=5;
+		Integer y=6;
+		Integer z=8;
+		Float xf=6.6f,yf=8.8f,zf=7.7f;
+		String xStr="Apple",yStr="Peach",zStr="Banana";
+
+
+		//calling method for integer values
+		new MaximumTest(x,y,z).maximum();
+		//calling method for float values
+		new MaximumTest(xf,yf,zf).maximum();
+		//calling method for String values
+		MaximumTest.testMaximum(xStr,yStr,zStr);
 
 	}
+
+	
+	
 }
