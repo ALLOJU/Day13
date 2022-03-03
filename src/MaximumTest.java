546126acd4feec06272c448402008061a9be5c6b
ALLOJU <mounika.alloju@gmail.com>
2022-03-03 10:23:25 +0530

    UC3 with generic class added

--- src/MaximumTest.java
+++ src/MaximumTest.java
@@ -1,13 +1,22 @@
-public class MaximumTest {
+public class MaximumTest<T extends Comparable<T>> {
 
-	//Method to find maximum of 3 number
+	//declaring variables
+	public T x,y,z;
+
+
+	//creating constructor
+	public MaximumTest(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 	/*
 	 * Creating generic method for accepting different data types
 	 * 
 	 * */
-	public static<T extends Comparable<T>> void testMaximum(T x,T y,T z) {
+	public static<T extends Comparable<T>> T testMaximum(T x,T y,T z) {
 		T max = x;
-	
+
 		if(y.compareTo(max)>0) {
 			max=y;
 		}
@@ -16,18 +25,20 @@ public class MaximumTest {
 		}
 		printMax(x,y,z,max);
 		
+		return max;
 	}
 	public static<T> void printMax(T x, T y, T z, T max) {
 		System.out.printf("Max of %s ,%s and %s is %s \n",x,y,z,max);		
 	}
-	
+
 
 	public static void main(String[] args) {
-
+	
 		//declaring values
 		Integer x=5, y=6,z=8;
 		Float xf=6.6f,yf=8.8f,zf=7.7f;
 		String xStr="Apple",yStr="Peach",zStr="Banana";
+		
 		MaximumTest.testMaximum(x,y,z);
 		MaximumTest.testMaximum(xf, yf, zf);
 		MaximumTest.testMaximum(xStr, yStr, zStr);
